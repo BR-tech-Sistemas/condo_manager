503d8302940ed15bbdaeef002f1cebdffcfc9edb
Braiani <felipe.braianitech@gmail.com>
2024-03-03 17:14:24 -0400

    Changer table's type from string to longtext

--- database/migrations/2024_03_02_193651_create_annoucements_table.php
+++ database/migrations/2024_03_02_193651_create_annoucements_table.php
@@ -10,7 +10,7 @@ return new class extends Migration {
         Schema::create('announcements', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $table->string('description');
+            $table->longText('description');
             $table->boolean('show_dashboard')->default(false);
             $table->string('priority');
             $table->date('valid_until')->nullable();
